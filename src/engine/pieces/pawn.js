71ec6f1b1f04823ce5aacedf51f1d9d6db2c0f44
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-10-06 15:37:16 +0100

    Test Passed Red-18

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -1,6 +1,7 @@
 import Player from "../player"
 import Square from "../square"
 import Piece from "./piece"
+import King from "./king"
 
 export default class Pawn extends Piece {
 	constructor(player) {
@@ -9,34 +10,72 @@ export default class Pawn extends Piece {
 
 	getAvailableMoves(board) {
 		const moves = []
-		let direction = (this.player === Player.WHITE) ? 1 : -1;
-        let startingPawnRow = (this.player === Player.WHITE) ? 1 : 6;
+		let direction = this.player === Player.WHITE ? 1 : -1
+		let startingPawnRow = this.player === Player.WHITE ? 1 : 6
+		let lastPawnRow = this.player === Player.WHITE ? 7 : 0
 
-        // Get the Pawn's current location
-		let location = board.findPiece(this);
+		// Get the Pawn's current location
+		let location = board.findPiece(this)
 
-        //Get the square one space in front
-        let squareInFront = Square.at(location.row + direction, location.col);
-        // Check that square for pieces
-        let pieceInFront = board.getPiece(squareInFront);
+		// Prevent movement at end of board
+		if (location.row === lastPawnRow) {
+			return []
+		}
 
-        if (!pieceInFront){
-            // Square in front is a valid move
-            moves.push(squareInFront);
-            // If the Pawn is in the starting row it can move 2 squares
-            if (location.row === startingPawnRow) {
+		//Get the square one space in front
+		let squareInFront = Square.at(location.row + direction, location.col)
+		// Check that square for pieces
+		let pieceInFront = board.getPiece(squareInFront)
 
-                // Find the square two spaces away
-                let nextSquareInFront = Square.at(location.row + 2 * direction, location.col);
+		if (!pieceInFront) {
+			// Square in front is a valid move
+			moves.push(squareInFront)
+			// If the Pawn is in the starting row it can move 2 squares
+			if (location.row === startingPawnRow) {
+				// Find the square two spaces away
+				let nextSquareInFront = Square.at(
+					location.row + 2 * direction,
+					location.col
+				)
 
-                // Check the square for pieces
-                let nextPieceInFront = board.getPiece(nextSquareInFront);
-                if (!nextPieceInFront){
-                    // Square two pieces in front is a valid move
-                    moves.push(nextSquareInFront);
-                }
+				// Check the square for pieces
+				let nextPieceInFront = board.getPiece(nextSquareInFront)
+				if (!nextPieceInFront) {
+					// Square two pieces in front is a valid move
+					moves.push(nextSquareInFront)
+				}
+			}
+		}
+
+		// Check diagonal for opposing piece
+
+        // Diagonal left
+		if (location.col !== 0) {
+			const diagonalMoveLeft = Square.at(
+				location.row + direction,
+				location.col - 1
+			)
+            const pieceInDiagonalLeft = board.getPiece(diagonalMoveLeft);
+
+            // If there is a piece in the square and it belongs to opposing player
+            if(pieceInDiagonalLeft && (pieceInDiagonalLeft.player !== this.player) && !(pieceInDiagonalLeft instanceof King)){
+                moves.push(diagonalMoveLeft);
             }
-        }
+		}
+
+        // Diagonal right
+		if (location.col !== 7) {
+			const diagonalMoveRight = Square.at(
+				location.row + direction,
+				location.col + 1
+			)
+            const pieceInDiagonalRight = board.getPiece(diagonalMoveRight);
+            // Check not a King Piece
+            // If there is a piece in the square and it belongs to opposing player
+            if(pieceInDiagonalRight && (pieceInDiagonalRight.player !== this.player) && !(pieceInDiagonalRight instanceof King)){
+                moves.push(diagonalMoveRight);
+            }
+		}
 
 		return moves
 	}
