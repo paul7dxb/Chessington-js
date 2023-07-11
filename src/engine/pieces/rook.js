59afd043d2100b93e39942cb453df83db2e717bc
unknown <paul7markets@hotmail.com>
2023-07-11 14:13:30 +0100

    Fixed test red 3

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,11 +1,31 @@
-import Piece from './piece';
+import Piece from "./piece"
+import Square from "../square"
+
 
 export default class Rook extends Piece {
-    constructor(player) {
-        super(player);
-    }
+	constructor(player) {
+		super(player)
+	}
 
-    getAvailableMoves(board) {
-        return new Array(0);
-    }
+	getAvailableMoves(board) {
+		let moves = []
+
+		let location = board.findPiece(this)
+
+		// Add moves in row
+		for (let i = 0; i < 8; i++) {
+			if (location.col !== i) {
+				moves.push(Square.at(location.row, i))
+			}
+		}
+
+		// Add moves in column
+		for (let i = 0; i < 8; i++) {
+			if (location.row !== i) {
+				moves.push(Square.at(i, location.col))
+			}
+		}
+
+		return moves
+	}
 }
