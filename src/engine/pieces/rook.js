6ba9390bd10652265f02dd645d1f00388ed4d9ff
Akla Djankla <djsolangea@gmail.com>
2023-07-12 14:44:31 +0100

    test passed Red-9

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -11,21 +11,32 @@ export default class Rook extends Piece {
 		let moves = []
 
 		let location = board.findPiece(this)
+		let rookDirections = [
+			{row:0, col:-1 }, {row:0, col:1 }, {row:1, col:0 },{row:-1, col:0 }
+		];
 
-		// Add moves in row
-		for (let i = 0; i < 8; i++) {
-			if (location.col !== i) {
-				moves.push(Square.at(location.row, i))
-			}
+		// find how far piece can go in each direction
+		
+		for (let rookDirection of rookDirections){
+			for( let i = 1; i < 8; i++ ){
+				let potentialSquare = Square.at(location.row + rookDirection.row * i, location.col + rookDirection.col * i);
+				if(potentialSquare.row >= 0 && potentialSquare.row <= 7 && potentialSquare.col >= 0 && potentialSquare.col <= 7){
+					let piece = board.getPiece(potentialSquare);
+					if(!piece){
+						moves.push(potentialSquare);
+						continue;
+					}
+					if(piece.player !== this.player){
+						moves.push(potentialSquare);
+					}
+					break;
+				}
+				else {break}
+			}	
+			
 		}
 
-		// Add moves in column
-		for (let i = 0; i < 8; i++) {
-			if (location.row !== i) {
-				moves.push(Square.at(i, location.col))
-			}
-		}
-
+		
 		return moves
 	}
 }
